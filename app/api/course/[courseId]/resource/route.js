68dcb62ad687fe592914db82031a05d0f8a3a90f
S.M. Ashfak Uddin <smashfakuddin.anthro.cou@gmail.com>
2025-08-30 20:42:14 +0600

    resource crud opertion added

--- app/api/course/[courseId]/resource/route.js
+++ app/api/course/[courseId]/resource/route.js
@@ -47,3 +47,79 @@ export async function POST(request, { params }) {
     );
   }
 }
+
+export async function PUT(request, { params }) {
+  await dbConnect();
+  try {
+    const { title, url, description, resourceId } = await request.json();
+
+    if (!resourceId) {
+      return Response.json(
+        { message: "Resource ID is required" },
+        { status: 400 }
+      );
+    }
+
+    // Find and update resource
+    const updatedResource = await Resource.findByIdAndUpdate(
+      resourceId,
+      { title, url, description },
+      { new: true }
+    );
+
+    if (!updatedResource) {
+      return Response.json({ message: "Resource not found" }, { status: 404 });
+    }
+
+    return Response.json(
+      { message: "Resource updated successfully", updatedResource },
+      { status: 200 }
+    );
+  } catch (error) {}
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    await dbConnect();
+
+    // Parse request body
+    const { resourceId } = await request.json();
+    const { courseId } = await params;
+
+    if (!resourceId || !courseId) {
+      return new Response.json(
+        { message: "Resource ID and Course ID are required" },
+        { status: 400 }
+      );
+    }
+
+    // Delete resource by ID
+    const deletedResource = await Resource.findByIdAndDelete(resourceId);
+
+    if (!deletedResource) {
+      return Response.json(
+        { message: "Resource not found" },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    // Remove resource from course
+    await Course.findByIdAndUpdate(courseId, {
+      $pull: { resource: resourceId },
+    });
+
+    return Response.json(
+      {
+        message: "Resource deleted successfully",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return Response.json(
+      { message: "Something went wrong", error: error.message },
+      { status: 500 }
+    );
+  }
+}
